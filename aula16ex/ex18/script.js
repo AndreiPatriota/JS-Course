6b31e758c0ad0c99e74abdc7b8dba8e10090c2e7
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2023-02-08 23:47:49 -0300

    Clear some code
    Cleaning code for fun

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -1,71 +1,68 @@
-const lstOfVals = [] //Array that will store the entered values
-const divRes = document.querySelector('div#res') //Html div element that holds the output display
+const listofNumbers = [Number()] //Array that will store the entered values
+listofNumbers.length = 0
+const divElement = document.querySelector('div#res') //Html div element that holds the output display
 
-function addNum2Array(){/*addNum2Array() starts here*/
-    let txtBox = document.querySelector('input#inNum') //Html input element
-    let rawVal = txtBox.value //Raw input value
+function addNum2Array() {/*addNum2Array() starts here*/
+    let inputElement = document.querySelector('input#inNum')
+    let rawValue = inputElement.value
 
-    if(rawVal.length == 0){//if no number has been entered, show this and beat it
+    //Clears input
+    inputElement.value = ''
+    inputElement.focus()
+
+    if (rawValue.length == 0) {
         alert('Field is empty. Please, insert a valid number')
-    }else{//else, do the thing
-        let numVal = Number(rawVal)//converts raw input to numerical value
-
-        if(numVal >= 1 && numVal <= 100){//checks if the entered value is between 1 and 100
-            if(lstOfVals.indexOf(numVal) == -1){//checks if the number has already been added to the list
-                lstOfVals.push(numVal)//Inserts number in the list
-
-                /*Creates an Html option element, and appends it to the
-                Html select element*/
-                let optionItem = document.createElement('option')
-                optionItem.value = numVal
-                optionItem.innerHTML = `Number ${numVal} has been added.`
-                document.querySelector('select#selRes').appendChild(optionItem)
-
-                divRes.innerHTML = ''//Clears the output display area
-            }else{//In case the number has already been added, show this and leave
-                alert('Number has already been added to the list')
-            }
-        }else{//In case the number is outside the range, show this and get lost
-            alert('Input is out of range. Please, inserta a number between 1 and 100!')
-        }
-        /*Regardless of the number being valid or not,
-        always clear the input element entry*/
-        txtBox.value = ''
-        txtBox.focus()
+        return
     }
-    /*addNum2Array() ends here*/
+
+    let numericalValue = Number(rawValue)
+
+    if (numericalValue < 1 || numericalValue > 100) {
+        alert('Input is out of range. Insert a number between 1 and 100!')
+        return
+    }
+
+    if (listofNumbers.indexOf(numericalValue) != -1) {
+        alert('Number has already been added to the list')
+        return
+    }
+
+    listofNumbers.push(numericalValue)
+
+    let optionElement = document.createElement('option')
+    optionElement.value = numericalValue
+    optionElement.innerHTML = `Number ${numericalValue} has been added.`
+    document.querySelector('select#selRes').appendChild(optionElement)
+
+    divElement.innerHTML = ''//Clears the output display area
 }
 
-function printRes(){/*printRes() starts here*/
-    if(lstOfVals.length != 0){//Check if the list is empty
-        let lstSize = lstOfVals.length //Gets the current size of the list
-        
-        /*Initializes the maximum and minimum values,
-        and the summation accumulator*/
-        let max = lstOfVals[0]
-        let min = lstOfVals[0]
-        let sum = 0
+function printRes() {/*printRes() starts here*/
 
-        /*loops through the list to find 
-        the smallest and biggest numbers, and 
-        to obtain the elements summation*/
-        for(let pos in lstOfVals){
-            max = lstOfVals[pos] > max ? lstOfVals[pos] : max
-            min = lstOfVals[pos] < min ? lstOfVals[pos] : min
-            sum += lstOfVals[pos]
-        }
-
-        let average = sum / lstSize//Calculates the average of the list's elements
-        
-        /*Presets the list's specs in the output
-        display area*/
-        divRes.innerHTML = `<p>The list has ${lstSize} numbers.</p>`
-        divRes.innerHTML += `<p>The biggest number so far is ${max}.</p>`
-        divRes.innerHTML += `<p>The lowest number so far is ${min} values.</p>`
-        divRes.innerHTML += `<p>The summation of the lists elements equals ${sum}.</p>`
-        divRes.innerHTML += `<p>The average of the lists elements equals ${average}</p>`
-    }else{//In case list is empty, show this, and leave
+    if(listofNumbers.length == 0) {
         alert('There are no stored values in the list.')
+        return
     }
-    /*printRes() ends here*/ 
+
+    let listSize = listofNumbers.length 
+
+    //Initializations
+    let max = Number(listofNumbers[0])
+    let min = Number(listofNumbers[0])
+    let sum = 0
+
+    listofNumbers.forEach((value)=>{
+        max = value > max ? value : max;
+        min = value < min ? value : min;
+        sum += value;
+    })
+
+    let average = sum / listSize
+
+    //Shows output
+    divElement.innerHTML = `<p>The list has ${listSize} numbers.</p>`
+    divElement.innerHTML += `<p>The biggest number so far is ${max}.</p>`
+    divElement.innerHTML += `<p>The lowest number so far is ${min}.</p>`
+    divElement.innerHTML += `<p>The summation of the lists elements equals ${sum}.</p>`
+    divElement.innerHTML += `<p>The average of the lists elements equals ${average}</p>`
 }
